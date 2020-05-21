a796eb07d923bb3d8828211ae43224baeb8acad5
acorn-527 <sollee2203@gmail.com>
2020-05-21 19:29:56 +0900

    2020-05-21

---
+++ src/study/Customer.java
@@ -0,0 +1,10 @@
+package study;
+
+public class Customer {
+	String name;
+	public Customer(String n) {
+		name = n;
+	
+	
+	}
+}
---
+++ src/study/Java3.java
@@ -0,0 +1,89 @@
+package study;
+
+public class Java3 {
+	public static void main(String[] args) {
+		//1. 배열 선언과 생성
+		int[] a = new int[4];
+				 
+		//2. 할당
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		
+		//3. 출력
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		
+		int a1 = a[1];
+		int a2 = a[2];
+		int sum = a[1] + a[2];
+		//*int sum = a1 + a2 이거 보다 위에 바로 하는 게 더 좋음.
+		
+		System.out.println(sum);
+	
+		//출력 for loop
+		for(int i=0 ; i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		//출력 enhanced for loop
+		
+		for(int value : a) {
+			System.out.println(value);
+		
+		}
+		//1. 배열 선언, 생성, 할당
+		String[] str = {"apple","banana","melon","blueberry"}; //{과일}앞에 "String[]" 안해도됌.
+		
+		//2. 출력 for loop
+		for(int i=0; i<str.length;i++) {
+			String value = str[i];
+			System.out.println(value);
+		}			
+		
+		//3. 출력 enhanced for loop
+		for(String value : str) {
+			System.out.println(value);
+	
+		}
+		
+		
+		//*(몰라도 되는데 배열이름 설정하는 거!) 참조에 참조한거야 value값은 이상한데 value.name해야 이름이나와. 
+		Customer[] cust = new Customer[2];
+				
+		cust[0] = new Customer("성영한");
+		cust[1] = new Customer("손정의");
+				
+		for( Customer value : cust ) {
+			System.out.println(value.name);
+		}
+		
+		int[] array = new int[4];
+		 array[0] = 100;
+		 array[1] = 200;
+		 array[2] = 300;
+		 array[3] = 400;
+		 
+		 for(int i=0; i<array.length;i++) {
+			 System.out.println(array[i]);
+		 }
+		 
+		 for(int value : array) {
+		 System.out.println(value);
+		 }
+		 
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
